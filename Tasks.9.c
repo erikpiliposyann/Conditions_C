8b123c976416d91e4cd4107df555676e0c4dc8ec
erikpiliposyann <erikpiliposyann@gmail.com>
2026-06-09 23:06:56 +0400

    Add FizzBuzz program in Tasks.9.c

---
+++ Tasks.9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	printf("Input  number: ");
+	scanf("%i", &num);
+	
+	if((num % 3 == 0) && (num % 5 == 0)){
+		printf("FizzBuzz\n");
+	}
+	else if(num % 3 == 0){
+		printf("Fizz\n");
+	}
+	else if(num % 5 == 0){
+		printf("Buzz\n");
+	}
+	else{
+		printf("Nothing");
+	}
+}
